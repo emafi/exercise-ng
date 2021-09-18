df41d59820c9e717eb7b2e636f6a75d5cf91d273
emafi <emanuela.figl@gmail.com>
2021-09-18 21:35:50 +0200

    added/components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './components/user-container/user-container.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserSingleComponent } from './components/user-single/user-single.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { OrderContainerComponent } from './components/order-container/order-container.component';
+import { OrderListComponent } from './components/order-list/order-list.component';
+import { OrderSingleComponent } from './components/order-single/order-single.component';
+import { OrderDetailComponent } from './components/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    OrderContainerComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule,
